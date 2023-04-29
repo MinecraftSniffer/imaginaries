0505b365c75749534bb61748f9f0c6af33a1f530
MinecraftSniffer <130071632+MinecraftSniffer@users.noreply.github.com>
2023-04-29 19:59:19 +0800

    Update imaginary.sh

--- imaginary.sh
+++ imaginary.sh
@@ -1,10 +1,10 @@
 DISTRO_NAME="Imaginary OS v1.0 cookie"
 DISTRO_COMMENT="You need to use your imaginary for this.. jk"
 
-TARBALL_URL['arm']="https://github.com/MinecraftSniffer/imaginaries/releases/download/imaginary-releases/imaginary-arm-pd-v1.0.1.tar.xz"
-TARBALL_SHA256['arm']="8a06d82c2dc5e4f735c744725c86105986e4a8bc984207ccfb634943d3bc4644"
+TARBALL_URL['arm']="https://github.com/MinecraftSniffer/imaginaries/releases/download/imaginary-releases/imaginary-arm-pd-v1.0.2.tar.xz"
+TARBALL_SHA256['arm']="cf3e9f01751c90ffb791851568e8aa24e6839e87b8e554c81f82d03939da9885"
 
-TARBALL_URL['aarch64']="https://cmd.os/downloads/imaginary-aarch64-pd-v1.0.1.tar.xz"
+TARBALL_URL['aarch64']="https://github.com/MinecraftSniffer/imaginaries/releases/download/imaginary-aarch64-pd-v1.0.1.tar.xz"
 TARBALL_SHA256['aarch64']="b6e9c3975587598f4595612361574755f3311c593c85fe87c0c410dafa75d697"
 
 distro_setup() {
